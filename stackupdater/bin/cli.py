a8ff2817ad69965fd991455a111906a5c5cf837f
Oleg Levitsky <levabd@gmail.com>
2016-04-26 13:31:52 +0600

    fix cli params - add recipes path - fix zero input params error

--- stackupdater/bin/cli.py
+++ stackupdater/bin/cli.py
@@ -19,8 +19,7 @@ class Cli(object):
         """
         parser = argparse.ArgumentParser(description='Check dependencies by recipe.')
         parser.add_argument('action', choices=['get', 'install', 'check'], default='check', nargs=1, help='get: Download recipe from Gist; \ninstall: Trying to install newer package versions; \ncheck: Simple check')
-        parser.add_argument('gist_id', nargs='*', help='Gist ID to download recipe')
-        parser.add_argument('recipes', nargs='*', help='Custom recipe directory')
+        parser.add_argument('param', nargs='?', help='Custom recipe directory or gist ID to download recipe')
         parser.add_argument("-v", "--verbose", action="store_true", help="With this flag you can see on the display(not in the log file) triggered command output.")
 
         self.__cli_args = parser.parse_args()
@@ -39,7 +38,7 @@ class Cli(object):
         Download recipe from Gist
         """
         if len(self.__cli_args.gist_id) > 0: #ID of Gist not empty
-            recipe_downloader = Downloader(self.__base_path, self.__cli_args.gist_id[0], self.__cli_args.recipes[0])
+            recipe_downloader = Downloader(self.__base_path, self.__cli_args.param)
             recipe_downloader.install_gist()
         else:
             logger.error('Gist id not defined')
@@ -48,7 +47,7 @@ class Cli(object):
         """
         Check dependencies
         """
-        checker = Dependency(self.__base_path, self.__cli_args.recipes[0])
+        checker = Dependency(self.__base_path, self.__cli_args.param)
         exit_code = checker.check()
 
         sys.exit(exit_code)
@@ -57,7 +56,7 @@ class Cli(object):
         """
         Install dependencies
         """
-        installer = Dependency(self.__base_path, self.__cli_args.recipes[0])
+        installer = Dependency(self.__base_path, self.__cli_args.param)
         exit_code = installer.install(self.__cli_args.verbose)
 
         sys.exit(exit_code)
